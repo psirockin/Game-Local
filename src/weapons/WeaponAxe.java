04555638ecd4935a52ff688268ea16ccfacdd4a0
Megusuno <nexon226@gmail.com>
2015-04-07 23:01:42 -0700

    Update WeaponAxe.java

--- src/weapons/WeaponAxe.java
+++ src/weapons/WeaponAxe.java
@@ -5,7 +5,7 @@ public class WeaponAxe extends Weapon {
 	of the weapon, rather than its type, and two weapons of the same type may have their own unique names and stats.
 	In other words, only the weapon type will be excluded from the class parameters since that's the only one that matters.**/
 	public WeaponAxe(String name, int dur, int rang,int hit, int crit, int damage) {
-		super(name, dur, rang, hit, crit, damage, WeaponType_Axe);
+		super(name, dur, 1, hit, crit, damage, WeaponType_Axe);
 		// TODO Auto-generated constructor stub
 	}
 	
