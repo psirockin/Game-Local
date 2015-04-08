1582c77f05aca1d39cd653c8eb844fd088683a7a
Megusuno <nexon226@gmail.com>
2015-04-07 23:02:25 -0700

    Update WeaponTome.java

--- src/weapons/WeaponTome.java
+++ src/weapons/WeaponTome.java
@@ -2,6 +2,6 @@ package weapons;
 /**Tomes are a bit special. You can use tomes at range 1 AND 2. */
 public class WeaponTome extends Weapon {
 	public WeaponTome(String name, int dur, int rang,int hit, int crit, int damage) {
-		super(name, dur, rang, hit, crit, damage, WeaponType_Magic);
+		super(name, dur, 2, hit, crit, damage, WeaponType_Magic);
 	}
 }
