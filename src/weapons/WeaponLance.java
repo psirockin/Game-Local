dad1e909feb8cc09fcd37c1b28360ec33668e6f6
Megusuno <nexon226@gmail.com>
2015-04-07 23:01:55 -0700

    Update WeaponLance.java

--- src/weapons/WeaponLance.java
+++ src/weapons/WeaponLance.java
@@ -1,6 +1,6 @@
 package weapons;
 public class WeaponLance extends Weapon {
 	public WeaponLance(String name, int dur, int rang,int hit, int crit, int damage) {
-		super(name, dur, rang, hit, crit, damage, WeaponType_Lance);
+		super(name, dur, 1, hit, crit, damage, WeaponType_Lance);
 	}
 }
