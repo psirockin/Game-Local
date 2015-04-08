0e6c0aa13dfcfc89d1ae7b6fcaf4c54399808413
Megusuno <nexon226@gmail.com>
2015-04-07 23:02:11 -0700

    Update WeaponSword.java

--- src/weapons/WeaponSword.java
+++ src/weapons/WeaponSword.java
@@ -1,6 +1,6 @@
 package weapons;
 public class WeaponSword extends Weapon {
 	public WeaponSword(String name, int dur, int rang,int hit, int crit, int damage) {
-		super(name, dur, rang, hit, crit, damage, WeaponType_Sword);
+		super(name, dur, 1, hit, crit, damage, WeaponType_Sword);
 	}
 }
