b9a5461f2b11c9a831127932a6f5f3976ad04b33
Megusuno <nexon226@gmail.com>
2015-04-07 23:01:02 -0700

    Update WeaponAxe.java

--- src/weapons/WeaponAxe.java
+++ src/weapons/WeaponAxe.java
@@ -1,7 +1,9 @@
 package weapons;
 import weapons.Weapon;
 public class WeaponAxe extends Weapon {
-
+	/*I just realized that the names don't really have to be changed; this is because the string name is for the name
+	of the weapon, rather than its type, and two weapons of the same type may have their own unique names and stats.
+	In other words, only the weapon type will be excluded from the class parameters since that's the only one that matters.**/
 	public WeaponAxe(String name, int dur, int rang,int hit, int crit, int damage) {
 		super(name, dur, rang, hit, crit, damage, WeaponType_Axe);
 		// TODO Auto-generated constructor stub
